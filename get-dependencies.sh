3b89a0ff139f167d3f834700763b84314176ce51
Link4Electronics <link4electronics@gmail.com>
2026-03-18 02:37:59 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,32 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake    \
+    libdecor \
+    sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package libsmacker
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of lba2 community..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/LBALab/lba2-classic-community"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./lba2-community
+echo "$VERSION" > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./lba2-community
+mkdir -p build && cd build
+cmake .. \
+      -DSOUND_BACKEND=sdl \
+      -DMVIDEO_BACKEND=smacker \
+      -DCONSOLE_MODULE=ON
+make -j$(nproc)
+mv -v SOURCES/lba2 ../AppDir/bin
