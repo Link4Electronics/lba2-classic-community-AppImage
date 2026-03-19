94fbed6a873c6b5c2bce6adc2b1d745f1b117066
Link4Electronics <link4electronics@gmail.com>
2026-03-19 01:40:21 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,7 +28,6 @@ echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./lba2-community
-#patch -Np1 -i ../rename-symbol.patch
 mkdir -p build && cd build
 cmake .. \
       -DSOUND_BACKEND=sdl \
